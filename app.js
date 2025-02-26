6280f31c1848e055749254625841c929b49ceeeb
Lucas Araújo <samuell.llucas02@gmail.com>
2025-02-26 14:47:20 -0300

    app.js

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
-function sortear() {
-  let quantidade = parseInt(document.getElementById('quantidade').value);
-  let de = parseInt(document.getElementById('de').value);
-  let ate = parseInt(document.getElementById('ate').value);
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
-  if (de >= ate) {
-    alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
-    return;
-  }
+    let sorteados = [];
+    let numero;
 
-  if (quantidade > (ate - de + 1)) {
-    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
-    return;
-  }
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
 
-  let sorteados = [];
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+            alert('Tentando obter número inédito');
+        }
 
-  for (let i = 0; i < quantidade; i++) {
-    let numero = obterNumeroAleatorio(de, ate);
-
-    while (sorteados.includes(numero)) {
-      numero = obterNumeroAleatorio(de, ate);
-      alert('Tentando obter número inédito');
+        sorteados.push(numero);
     }
 
-    sorteados.push(numero);
-  }
-
-  let resultado = document.getElementById('resultado');
-  resultado.textContent = `Números sorteados: ${sorteados.join(', ')}`;
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+    alterarStatusBotao();
 }
 
-function obterNumeroAleatorio(de, ate) {
-  return Math.floor(Math.random() * (ate - de + 1)) + de;
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = 'Números sorteados:  nenhum até agora';
+    alterarStatusBotao();
 }
